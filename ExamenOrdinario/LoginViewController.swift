c296e6643edcbb6e0dd68c38783d71c5e6301a53
conri <cdelsogonzalez@gmail.com>
2018-06-19 13:07:04 +0200

    se me habia olvidado meter el archivo de google

--- ExamenOrdinario/LoginViewController.swift
+++ ExamenOrdinario/LoginViewController.swift
@@ -6,4 +6,39 @@
 //  Copyright © 2018 CONRADO DELSO GONZALEZ. All rights reserved.
 //
 
-import Foundation
+import UIKit
+import Firebase
+
+class LoginViewController: UIViewController {
+    @IBOutlet weak var loginButton: UIButton!
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        loginButton.layer.borderWidth = 2
+        loginButton.layer.cornerRadius = 10
+        loginButton.layer.borderColor = UIColor.white.cgColor
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+    
+    @IBAction func loginButtonPressed(_ sender: Any) {
+        Auth.auth().signIn(withEmail: emailTextField.text!, password: passwordTextField.text!) { (user, error) in
+            if error != nil {
+                print(error!)
+            }else{
+                print("login successful")
+                self.performSegue(withIdentifier: "goToMain", sender: self)
+            }
+        }
+        
+    }
+    
+}
--- ExamenOrdinario/MainViewController.swift
+++ ExamenOrdinario/MainViewController.swift
@@ -6,4 +6,37 @@
 //  Copyright © 2018 CONRADO DELSO GONZALEZ. All rights reserved.
 //
 
-import Foundation
+import UIKit
+import Firebase
+
+class MainViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Do any additional setup after loading the view.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+    @IBAction func logOutButtonPressed(_ sender: UIButton) {
+        do {
+            
+            try Auth.auth().signOut()
+        }catch{
+            print("there was a problem signing out")
+        }
+        
+        guard(navigationController?.popToRootViewController(animated: true)) != nil
+            
+            else{
+                print("You are signed out")
+                return
+        }
+    }
+    
+}
