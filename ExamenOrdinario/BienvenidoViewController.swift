1ed0aca33e1bd9dfea8719b3820bbc81b69ebf3b
conri <cdelsogonzalez@gmail.com>
2018-06-19 12:28:38 +0200

    algunos arreglos

--- ExamenOrdinario/BienvenidoViewController.swift
+++ ExamenOrdinario/BienvenidoViewController.swift
@@ -8,23 +8,27 @@
 
 import UIKit
 
-class BienvenidoViewController:UIViewControlle{
+class BienvenidoViewController: UIViewController {
     @IBOutlet weak var loginButton: UIButton!
+    @IBOutlet weak var registerButton: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         loginButton.layer.borderWidth = 2
-        loginButton.layer.cornerRadiusv = 10
+        loginButton.layer.cornerRadius = 10
         loginButton.layer.borderColor = UIColor.white.cgColor
         
+        registerButton.layer.borderWidth = 2
+        registerButton.layer.cornerRadius = 10
+        registerButton.layer.borderColor = UIColor.white.cgColor
+        
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    
 }
-
